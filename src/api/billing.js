07a71299703570a58c5242b1201a27528a346496
holagabriel.dev <holagabriel.dev@gmail.com>
2026-02-12 20:41:39 +0000

    Sistema de facturación mockeado

--- src/api/billing.js
+++ src/api/billing.js
@@ -1,11 +1,16 @@
 import { BASE_API_URL } from '../constants'
 import { authenticatedFetch } from './auth'
+
 export async function getBalance() {
-  const response = await authenticatedFetch(
-    `${BASE_API_URL}/api/billing/getBalance`
-  )
-  if (!response.ok) {
-    throw new Error(`Failed to fetch balance: ${response.status}`)
-  }
-  return await response.json()
+  // TODO: manejo de billing del lado del frontend
+  // const response = await authenticatedFetch(
+  //   `${BASE_API_URL}/api/billing/getBalance`
+  // )
+  // if (!response.ok) {
+  //   throw new Error(`Failed to fetch balance: ${response.status}`)
+  // }
+  // return await response.json()
+
+  // TODO: Resultado mock
+  return {"balance":"3.00000000"}
 }
