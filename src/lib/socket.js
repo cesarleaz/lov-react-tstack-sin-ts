563ee4a12331d339f3f2c712cb55b41ca38ad946
holagabriel.dev <holagabriel.dev@gmail.com>
2026-02-12 20:39:21 +0000

    format socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,7 @@
 import * as ISocket from '@/types/socket'
 import { io } from 'socket.io-client'
 import { eventBus } from './event'
+
 export class SocketIOManager {
   config
   socket = null
@@ -8,18 +9,23 @@ export class SocketIOManager {
   reconnectAttempts = 0
   maxReconnectAttempts = 5
   reconnectDelay = 1000
+
   constructor(config = {}) {
     this.config = config
+
     if (config.autoConnect !== false) {
       this.connect()
     }
   }
+
   connect(serverUrl) {
     return new Promise((resolve, reject) => {
       const url = serverUrl || this.config.serverUrl
+
       if (this.socket) {
         this.socket.disconnect()
       }
+
       this.socket = io(url, {
         transports: ['websocket'],
         upgrade: false,
@@ -27,16 +33,19 @@ export class SocketIOManager {
         reconnectionAttempts: this.maxReconnectAttempts,
         reconnectionDelay: this.reconnectDelay
       })
+
       this.socket.on('connect', () => {
         console.log('✅ Socket.IO connected:', this.socket?.id)
         this.connected = true
         this.reconnectAttempts = 0
         resolve(true)
       })
+
       this.socket.on('connect_error', (error) => {
         console.error('❌ Socket.IO connection error:', error)
         this.connected = false
         this.reconnectAttempts++
+
         if (this.reconnectAttempts >= this.maxReconnectAttempts) {
           reject(
             new Error(
@@ -45,86 +54,112 @@ export class SocketIOManager {
           )
         }
       })
+
       this.socket.on('disconnect', (reason) => {
         console.log('🔌 Socket.IO disconnected:', reason)
         this.connected = false
       })
+
       this.registerEventHandlers()
     })
   }
+
   registerEventHandlers() {
     if (!this.socket) return
+
     this.socket.on('connected', (data) => {
       console.log('🔗 Socket.IO connection confirmed:', data)
     })
+
     this.socket.on('init_done', (data) => {
       console.log('🔗 Server initialization done:', data)
     })
+
     this.socket.on('session_update', (data) => {
       this.handleSessionUpdate(data)
     })
+
     this.socket.on('pong', (data) => {
       console.log('🔗 Pong received:', data)
     })
   }
+
   handleSessionUpdate(data) {
     const { session_id, type } = data
+
     if (!session_id) {
       console.warn('⚠️ Session update missing session_id:', data)
       return
     }
+
     switch (type) {
       case ISocket.SessionEventType.Delta:
         eventBus.emit('Socket::Session::Delta', data)
         break
+
       case ISocket.SessionEventType.ToolCall:
         eventBus.emit('Socket::Session::ToolCall', data)
         break
+
       case ISocket.SessionEventType.ToolCallPendingConfirmation:
         eventBus.emit('Socket::Session::ToolCallPendingConfirmation', data)
         break
+
       case ISocket.SessionEventType.ToolCallConfirmed:
         eventBus.emit('Socket::Session::ToolCallConfirmed', data)
         break
+
       case ISocket.SessionEventType.ToolCallCancelled:
         eventBus.emit('Socket::Session::ToolCallCancelled', data)
         break
+
       case ISocket.SessionEventType.ToolCallArguments:
         eventBus.emit('Socket::Session::ToolCallArguments', data)
         break
+
       case ISocket.SessionEventType.ToolCallProgress:
         eventBus.emit('Socket::Session::ToolCallProgress', data)
         break
+
       case ISocket.SessionEventType.ImageGenerated:
         eventBus.emit('Socket::Session::ImageGenerated', data)
         break
+
       case ISocket.SessionEventType.VideoGenerated:
         eventBus.emit('Socket::Session::VideoGenerated', data)
         break
+
       case ISocket.SessionEventType.AllMessages:
         eventBus.emit('Socket::Session::AllMessages', data)
         break
+
       case ISocket.SessionEventType.Done:
         eventBus.emit('Socket::Session::Done', data)
         break
+
       case ISocket.SessionEventType.Error:
         eventBus.emit('Socket::Session::Error', data)
         break
+
       case ISocket.SessionEventType.Info:
         eventBus.emit('Socket::Session::Info', data)
         break
+
       case ISocket.SessionEventType.ToolCallResult:
         eventBus.emit('Socket::Session::ToolCallResult', data)
         break
+
       default:
         console.log('⚠️ Unknown session update type:', type)
     }
   }
+
   ping(data) {
     if (this.socket && this.connected) {
       this.socket.emit('ping', data)
     }
   }
+
   disconnect() {
     if (this.socket) {
       this.socket.disconnect()
@@ -133,18 +168,23 @@ export class SocketIOManager {
       console.log('🔌 Socket.IO manually disconnected')
     }
   }
+
   isConnected() {
     return this.connected
   }
+
   getSocketId() {
     return this.socket?.id
   }
+
   getSocket() {
     return this.socket
   }
+
   getReconnectAttempts() {
     return this.reconnectAttempts
   }
+
   isMaxReconnectAttemptsReached() {
     return this.reconnectAttempts >= this.maxReconnectAttempts
   }
